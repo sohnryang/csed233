5ff062bc4be45934284230ca09d99b2dfbfb31a2
Ryang Sohn <loop.infinitely@gmail.com>
2022-10-10 17:11:20 +0900

    Finish PA 2

--- assignments/pa2/heap.cpp
+++ assignments/pa2/heap.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+void swap(int &a, int &b) {
+  int tmp = a;
+  a = b;
+  b = tmp;
+}
+
 MinHeap::MinHeap() {
   heap_size = 0;
   heap_arr = new int[100]; // Default array of size 100
@@ -20,6 +26,15 @@ void MinHeap::insertKey(int k) {
   /////////////////////////////////////////////////////////
   //////////  TODO: Implement From Here      //////////////
 
+  heap_arr[heap_size] = k;
+  size_t current_index = heap_size++;
+  while (current_index != 0) {
+    size_t parent_index = (current_index + 1) / 2 - 1;
+    if (heap_arr[current_index] >= heap_arr[parent_index])
+      break;
+    swap(heap_arr[current_index], heap_arr[parent_index]);
+    current_index = parent_index;
+  }
 
   ///////////      End of Implementation      /////////////
   /////////////////////////////////////////////////////////
@@ -29,6 +44,8 @@ void MinHeap::deleteMin() {
   /////////////////////////////////////////////////////////
   //////////  TODO: Implement From Here      //////////////
 
+  heap_arr[0] = heap_arr[--heap_size];
+  minHeapProp(0);
 
   ///////////      End of Implementation      /////////////
   /////////////////////////////////////////////////////////
@@ -38,6 +55,24 @@ void MinHeap::minHeapProp(int i) {
   /////////////////////////////////////////////////////////
   //////////  TODO: Implement From Here      //////////////
 
+  int current_index = i;
+  while (true) {
+    int left_child_index = 2 * (current_index + 1) - 1,
+        right_child_index = 2 * (current_index + 1);
+    if (left_child_index >= heap_size && right_child_index >= heap_size)
+      break;
+    int new_index = current_index;
+    if (left_child_index < heap_size &&
+        heap_arr[left_child_index] < heap_arr[new_index])
+      new_index = left_child_index;
+    if (right_child_index < heap_size &&
+        heap_arr[right_child_index] < heap_arr[new_index])
+      new_index = right_child_index;
+    if (new_index == current_index)
+      break;
+    swap(heap_arr[current_index], heap_arr[new_index]);
+    current_index = new_index;
+  }
 
   ///////////      End of Implementation      /////////////
   /////////////////////////////////////////////////////////
@@ -47,6 +82,8 @@ void MinHeap::deleteElement(int i) {
   /////////////////////////////////////////////////////////
   //////////  TODO: Implement From Here      //////////////
 
+  heap_arr[i] = heap_arr[--heap_size];
+  minHeapProp(i);
 
   ///////////      End of Implementation      /////////////
   /////////////////////////////////////////////////////////
