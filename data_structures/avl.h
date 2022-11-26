0578e5c1a772d7d76589a85ca80a49e840620a76
Ryang Sohn <loop.infinitely@gmail.com>
2022-11-26 17:06:31 +0900

    Implement RR and LL rotations

--- data_structures/avl.h
+++ data_structures/avl.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <deque.h> // expand: true
+#include <utils.h> // expand: true
 
 template <typename K, typename V> struct AVLNode {
   K key;
@@ -19,6 +20,8 @@ public:
   ~AVLTree<K, V>();
   AVLNode<K, V> *get_root() const;
   AVLNode<K, V> *get(K key) const;
+  void ll_rotate(AVLNode<K, V> *node);
+  void rr_rotate(AVLNode<K, V> *node);
   void insert(K key, V value);
   void remove(K key);
 };
@@ -56,4 +59,48 @@ AVLNode<K, V> *AVLTree<K, V>::get(K key) const {
       current = current->right;
   }
   return current;
+}
+
+template <typename K, typename V>
+void AVLTree<K, V>::ll_rotate(AVLNode<K, V> *node) {
+  auto right_child = node->right, left_left_child = node->left->left,
+       left_right_child = node->left->right;
+  swap(node->key, node->left->key);
+  swap(node->value, node->left->value);
+  swap(node->left->left, node->right);
+  swap(node->left->left, node->left->right);
+  swap(node->left, node->right);
+  int left_left_height = -1;
+  if (left_left_child != nullptr)
+    left_left_height = left_left_child->height;
+  int left_right_height = -1;
+  if (left_right_child != nullptr)
+    left_right_height = left_right_child->height;
+  int right_height = -1;
+  if (right_child != nullptr)
+    right_height = right_child->height;
+  node->right->height = 1 + max(left_right_height, right_height);
+  node->height = 1 + max(node->right->height, left_left_height);
+}
+
+template <typename K, typename V>
+void AVLTree<K, V>::rr_rotate(AVLNode<K, V> *node) {
+  auto left_child = node->left, right_left_child = node->right->left,
+       right_right_child = node->right->right;
+  swap(node->key, node->right->key);
+  swap(node->value, node->right->value);
+  swap(node->right->right, node->left);
+  swap(node->right->left, node->right->right);
+  swap(node->left, node->right);
+  int left_height = -1;
+  if (left_child != nullptr)
+    left_height = left_child->height;
+  int right_left_height = -1;
+  if (right_left_child != nullptr)
+    right_left_height = right_left_child->height;
+  int right_right_height = -1;
+  if (right_right_child != nullptr)
+    right_right_height = right_right_child->height;
+  node->left->height = 1 + max(left_height, right_left_height);
+  node->height = 1 + max(node->left->height, right_right_height);
 }
