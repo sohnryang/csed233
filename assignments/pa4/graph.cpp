ece1b9c85d6af7f7d3304cae4737ac1af296db7c
Ryang Sohn <loop.infinitely@gmail.com>
2022-12-25 22:28:53 +0900

    Implement DFS and BFS on undirected graph

---
+++ assignments/pa4/graph.cpp
@@ -0,0 +1,244 @@
+#include "graph.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <deque.h> // expand: true
+#define INF 2147483647
+using namespace std;
+
+/////////////////////////////////////////////////////////
+//////////  TODO: Implement From Here      //////////////
+/*  Write your codes if you have additional functions  */
+
+int Graph::getNodeId(const string &label) {
+  int id;
+  if (label_id_table.count(label))
+    id = label_id_table[label];
+  else {
+    id = label_count++;
+    label_id_table[label] = id;
+    labels.push_back(label);
+    graph.push_back({});
+  }
+  return id;
+}
+
+void Graph::merge_arr(vector<Edge> &arr, int lo, int mid, int hi) {
+  std::vector<Edge> left_arr(mid - lo), right_arr(hi - mid);
+  for (int i = 0; i < mid - lo; i++)
+    left_arr[i] = arr[lo + i];
+  for (int i = 0; i < hi - mid; i++)
+    right_arr[i] = arr[mid + i];
+  int left_idx = 0, right_idx = 0, arr_idx = lo;
+  while (left_idx < mid - lo && right_idx < hi - mid) {
+    if (labels[left_arr[left_idx].id] < labels[right_arr[right_idx].id])
+      arr[arr_idx++] = left_arr[left_idx++];
+    else
+      arr[arr_idx++] = right_arr[right_idx++];
+  }
+  while (left_idx < mid - lo)
+    arr[arr_idx++] = left_arr[left_idx++];
+  while (right_idx < hi - mid)
+    arr[arr_idx++] = right_arr[right_idx++];
+}
+
+void Graph::sortByLabel(vector<Edge> &arr) {
+  for (int subarr_size = 1; subarr_size < arr.size(); subarr_size *= 2) {
+    for (int lo = 0; lo < arr.size(); lo += 2 * subarr_size) {
+      int mid = std::min(lo + subarr_size, (int)arr.size()),
+          hi = std::min(mid + subarr_size, (int)arr.size());
+      merge_arr(arr, lo, mid, hi);
+    }
+  }
+}
+
+void Graph::dfs_traverse(int here_id, vector<bool> &visited,
+                         vector<int> &visit_seq, vector<int> &finish_seq) {
+  visited[here_id] = true;
+  visit_seq.push_back(here_id);
+  for (int i = 0; i < graph[here_id].size(); i++) {
+    int there_id = graph[here_id][i].id;
+    if (!visited[there_id])
+      dfs_traverse(there_id, visited, visit_seq, finish_seq);
+  }
+  finish_seq.push_back(here_id);
+}
+
+void Graph::sortGraph() {
+  for (int i = 0; i < label_count; i++)
+    sortByLabel(graph[i]);
+}
+
+///////////      End of Implementation      /////////////
+/////////////////////////////////////////////////////////
+
+string Graph::DFS() {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+  vector<Edge> nodes(label_count);
+  for (int i = 0; i < label_count; i++)
+    nodes[i] = Edge(0, i);
+  sortByLabel(nodes);
+  sortGraph();
+  vector<bool> visited(label_count, false);
+  vector<int> visit_seq, finish_seq;
+  string result;
+  for (int i = 0; i < label_count; i++) {
+    int here_id = nodes[i].id;
+    if (visited[here_id])
+      continue;
+    if (!result.empty())
+      result += "\n";
+    dfs_traverse(here_id, visited, visit_seq, finish_seq);
+    for (int i = 0; i < visit_seq.size(); i++) {
+      if (i != 0)
+        result += " ";
+      result += labels[visit_seq[i]];
+    }
+    visit_seq.clear();
+  }
+  return result;
+  ///////////      End of Implementation      /////////////
+  ///////////////////////////////////////////////////////
+}
+
+string Graph::BFS() {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+  vector<Edge> nodes(label_count);
+  for (int i = 0; i < label_count; i++)
+    nodes[i] = Edge(0, i);
+  sortByLabel(nodes);
+  sortGraph();
+  vector<bool> visited(label_count, false);
+  string result;
+  for (int i = 0; i < label_count; i++) {
+    int start_id = nodes[i].id;
+    if (visited[start_id])
+      continue;
+    Deque<int> queue;
+    queue.push_back(start_id);
+    visited[start_id] = true;
+    string line = labels[start_id];
+    while (!queue.empty()) {
+      int here_id = queue.pop_front();
+      for (int j = 0; j < graph[here_id].size(); j++) {
+        int there_id = graph[here_id][j].id;
+        if (visited[there_id])
+          continue;
+        queue.push_back(there_id);
+        visited[there_id] = true;
+        line += " ";
+        line += labels[there_id];
+      }
+    }
+    if (!result.empty())
+      result += "\n";
+    result += line;
+  }
+  return result;
+  ///////////      End of Implementation      /////////////
+  ///////////////////////////////////////////////////////
+}
+
+int Graph::addDirectedEdge(string nodeA, string nodeB) {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+  addDirectedEdge(nodeA, nodeB, 0);
+  return 0;
+  ///////////      End of Implementation      /////////////
+  ///////////////////////////////////////////////////////
+}
+
+int Graph::addDirectedEdge(string nodeA, string nodeB, int weight) {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+  int nodeA_id = getNodeId(nodeA), nodeB_id = getNodeId(nodeB);
+  graph[nodeA_id].push_back(Edge(weight, nodeB_id));
+  return 0;
+  ///////////      End of Implementation      /////////////
+  ///////////////////////////////////////////////////////
+}
+
+int Graph::addUndirectedEdge(string nodeA, string nodeB) {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+  addUndirectedEdge(nodeA, nodeB, 0);
+  return 0;
+  ///////////      End of Implementation      /////////////
+  ///////////////////////////////////////////////////////
+}
+
+int Graph::addUndirectedEdge(string nodeA, string nodeB, int weight) {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+  addDirectedEdge(nodeA, nodeB, weight);
+  addDirectedEdge(nodeB, nodeA, weight);
+  return 0;
+  ///////////      End of Implementation      /////////////
+  ///////////////////////////////////////////////////////
+}
+
+int Graph::countUndirectedCycle() {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+
+  return 0;
+  ///////////      End of Implementation      /////////////
+  ///////////////////////////////////////////////////////
+}
+
+string Graph::getTopologicalSort() {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+
+  return "";
+  ///////////      End of Implementation      /////////////
+  ///////////////////////////////////////////////////////
+}
+
+int Graph::countDirectedCycle() {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+
+  return 0;
+  ///////////      End of Implementation      /////////////
+  ///////////////////////////////////////////////////////
+}
+
+string Graph::getShortestPath(string source, string destination) {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+
+  return "";
+  ///////////      End of Implementation      /////////////
+  /////////////////////////////////////////////////////////
+}
+
+string Graph::getAllShortestPaths() {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+
+  return "";
+  ///////////      End of Implementation      /////////////
+  /////////////////////////////////////////////////////////
+}
+
+int Graph::primMST(ofstream &fout, string startNode) {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+
+  return 0;
+  ///////////      End of Implementation      /////////////
+  /////////////////////////////////////////////////////////
+}
+
+int Graph::kruskalMST(ofstream &fout) {
+  /////////////////////////////////////////////////////////
+  //////////  TODO: Implement From Here      //////////////
+
+  return 0;
+  ///////////      End of Implementation      /////////////
+  /////////////////////////////////////////////////////////
+}
---
+++ assignments/pa4/graph.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <string>
+
+// TODO: remove training wheels
+#include <unordered_map>
+#include <vector>
+#define NodeMaxCount 101
+#define INF 2147483647
+using namespace std;
+
+/////////////////////////////////////////////////////////
+///  TODO: Add Your Struct or Functions if required /////
+
+struct Edge {
+  int weight, id;
+
+  Edge() : weight(0), id(0) {}
+  Edge(int weight, int id) : weight(weight), id(id) {}
+};
+
+///////////      End of Implementation      /////////////
+/////////////////////////////////////////////////////////
+
+class Graph {
+public:
+  Graph() : label_count(0), label_id_table(), labels(), graph{} {};
+  ~Graph(){};
+
+  string DFS();
+  void sortGraph();
+  string BFS();
+
+  int addDirectedEdge(string nodeA, string nodeB);
+  int addDirectedEdge(string nodeA, string nodeB, int weight);
+  int addUndirectedEdge(string nodeA, string nodeB);
+  int addUndirectedEdge(string nodeA, string nodeB, int weight);
+
+  int countUndirectedCycle();
+  string getTopologicalSort();
+  int countDirectedCycle();
+  string getShortestPath(string source, string destination);
+  string getAllShortestPaths();
+  int primMST(ofstream &, string startNode);
+  int kruskalMST(ofstream &);
+
+private:
+  /////////////////////////////////////////////////////////
+  //////  TODO: Add private members if required ///////////
+
+  int getNodeId(const string &label);
+  void sortByLabel(vector<Edge> &arr);
+  void merge_arr(vector<Edge> &arr, int lo, int mid, int hi);
+  void dfs_traverse(int here_id, vector<bool> &visited, vector<int> &visit_seq,
+                    vector<int> &finish_seq);
+
+  int label_count;
+  unordered_map<string, int> label_id_table;
+  vector<string> labels;
+  vector<vector<Edge>> graph;
+
+  ///////////      End of Implementation      /////////////
+  /////////////////////////////////////////////////////////
+};
